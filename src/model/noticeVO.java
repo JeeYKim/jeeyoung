28beaba955a5e6393db306fd1c039f250628e37d
JeeYKim <32816215+JeeYKim@users.noreply.github.com>
2017-10-18 00:53:08 +0900

    totalList&mainVO

---
+++ src/model/noticeVO.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.Date;
+
+public class noticeVO {
+
+	private int no;
+	private String id;
+	private String subject;
+	private String content;
+	private int count;
+	private Date regdate;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
+
+}
---
+++ src/model/recipeVO.java
@@ -0,0 +1,228 @@
+package model;
+
+import java.util.Date;
+
+public class recipeVO {
+	
+	private String id;                  //멤버아이디
+	   private int r_no;                  //레서피번호
+	   private String r_subject;            //제목
+	   private String r_category;         //레서피 카테고리
+	   private String r_count;               //조회수
+	   private Date r_regdate;            //등록일자
+	   private String r_content;         //레서피 요약 내용
+	   private String thumbnail;
+	   private String r_thumbnail;
+	   private String r_img1;
+	   private String r_img2;
+	   private String r_img3;
+	   private String r_img4;
+	   private String r_img5;
+	   private String r_img6;
+	   private String r_img7;
+	   private String r_img8;
+	   private String r_img9;
+	   private String r_com_img;
+	   private String r_text1;
+	   private String r_text2;
+	   private String r_text3;
+	   private String r_text4;
+	   private String r_text5;
+	   private String r_text6;
+	   private String r_text7;
+	   private String r_text8;
+	   private String r_text9;
+	   private Date b_regdate;
+	   private String r_ingr;
+	   private int r_totalgrade;
+	   
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getR_no() {
+		return r_no;
+	}
+	public void setR_no(int r_no) {
+		this.r_no = r_no;
+	}
+	public String getR_subject() {
+		return r_subject;
+	}
+	public void setR_subject(String r_subject) {
+		this.r_subject = r_subject;
+	}
+	public String getR_category() {
+		return r_category;
+	}
+	public void setR_category(String r_category) {
+		this.r_category = r_category;
+	}
+	public String getR_count() {
+		return r_count;
+	}
+	public void setR_count(String r_count) {
+		this.r_count = r_count;
+	}
+	public Date getR_regdate() {
+		return r_regdate;
+	}
+	public void setR_regdate(Date r_regdate) {
+		this.r_regdate = r_regdate;
+	}
+	public String getR_content() {
+		return r_content;
+	}
+	public void setR_content(String r_content) {
+		this.r_content = r_content;
+	}
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	public String getR_thumbnail() {
+		return r_thumbnail;
+	}
+	public void setR_thumbnail(String r_thumbnail) {
+		this.r_thumbnail = r_thumbnail;
+	}
+	public String getR_img1() {
+		return r_img1;
+	}
+	public void setR_img1(String r_img1) {
+		this.r_img1 = r_img1;
+	}
+	public String getR_img2() {
+		return r_img2;
+	}
+	public void setR_img2(String r_img2) {
+		this.r_img2 = r_img2;
+	}
+	public String getR_img3() {
+		return r_img3;
+	}
+	public void setR_img3(String r_img3) {
+		this.r_img3 = r_img3;
+	}
+	public String getR_img4() {
+		return r_img4;
+	}
+	public void setR_img4(String r_img4) {
+		this.r_img4 = r_img4;
+	}
+	public String getR_img5() {
+		return r_img5;
+	}
+	public void setR_img5(String r_img5) {
+		this.r_img5 = r_img5;
+	}
+	public String getR_img6() {
+		return r_img6;
+	}
+	public void setR_img6(String r_img6) {
+		this.r_img6 = r_img6;
+	}
+	public String getR_img7() {
+		return r_img7;
+	}
+	public void setR_img7(String r_img7) {
+		this.r_img7 = r_img7;
+	}
+	public String getR_img8() {
+		return r_img8;
+	}
+	public void setR_img8(String r_img8) {
+		this.r_img8 = r_img8;
+	}
+	public String getR_img9() {
+		return r_img9;
+	}
+	public void setR_img9(String r_img9) {
+		this.r_img9 = r_img9;
+	}
+	public String getR_com_img() {
+		return r_com_img;
+	}
+	public void setR_com_img(String r_com_img) {
+		this.r_com_img = r_com_img;
+	}
+	public String getR_text1() {
+		return r_text1;
+	}
+	public void setR_text1(String r_text1) {
+		this.r_text1 = r_text1;
+	}
+	public String getR_text2() {
+		return r_text2;
+	}
+	public void setR_text2(String r_text2) {
+		this.r_text2 = r_text2;
+	}
+	public String getR_text3() {
+		return r_text3;
+	}
+	public void setR_text3(String r_text3) {
+		this.r_text3 = r_text3;
+	}
+	public String getR_text4() {
+		return r_text4;
+	}
+	public void setR_text4(String r_text4) {
+		this.r_text4 = r_text4;
+	}
+	public String getR_text5() {
+		return r_text5;
+	}
+	public void setR_text5(String r_text5) {
+		this.r_text5 = r_text5;
+	}
+	public String getR_text6() {
+		return r_text6;
+	}
+	public void setR_text6(String r_text6) {
+		this.r_text6 = r_text6;
+	}
+	public String getR_text7() {
+		return r_text7;
+	}
+	public void setR_text7(String r_text7) {
+		this.r_text7 = r_text7;
+	}
+	public String getR_text8() {
+		return r_text8;
+	}
+	public void setR_text8(String r_text8) {
+		this.r_text8 = r_text8;
+	}
+	public String getR_text9() {
+		return r_text9;
+	}
+	public void setR_text9(String r_text9) {
+		this.r_text9 = r_text9;
+	}
+	public Date getB_regdate() {
+		return b_regdate;
+	}
+	public void setB_regdate(Date b_regdate) {
+		this.b_regdate = b_regdate;
+	}
+	public String getR_ingr() {
+		return r_ingr;
+	}
+	public void setR_ingr(String r_ingr) {
+		this.r_ingr = r_ingr;
+	}
+	public int getR_totalgrade() {
+		return r_totalgrade;
+	}
+	public void setR_totalgrade(int r_totalgrade) {
+		this.r_totalgrade = r_totalgrade;
+	}
+	   
+	   
+
+}
